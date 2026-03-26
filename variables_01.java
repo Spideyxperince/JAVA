08d3faa5fa6a0c8f4b8297ebcb3432f94dd81349
Spideyxperince <spideyxperience@gmail.com>
2026-03-26 23:32:01 +0530

    varaibles till print function

---
+++ variables_01.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class variables_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("this is a print function that creates new line");
+        // println creates a new line 
+        System.out.print("this print function also create a new line \n");
+        // here new line is created by \n (back_slash n)
+        System.out.print("this print function does not create a new line ");
+        
+
+        sc.close();
+    }
+}
