df286f4d02edf87b216f049c41894ed51d3eef90
Spideyxperince <spideyxperience@gmail.com>
2026-03-26 11:19:39 +0530

    MY FIRST HELLO WORLD PROG.

---
+++ First_01.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class First_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hello World , this is my first program");
+
+
+        sc.close();
+    }
+}
